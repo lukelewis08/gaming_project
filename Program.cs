bfb537bb9df869e7fb0f4399ec87d97535df96a3
Luke Lewis <luke.scott08@gmail.com>
2025-08-07 11:34:27 +0100

    add another print out

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("My name is Luke");
+        Console.WriteLine("Hello World!");
     }
 }
